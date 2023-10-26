405e58ae3988453d1d61ba452998bddda6d0db4e
Remi Werkmeister <remi.werkmeister@gmail.com>
2023-10-26 22:09:39 +0200

    Projet 1

--- Projet QCM/main.py
+++ Projet QCM/main.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     questionnaire = Questionnaire('Questions.json')  # Crée une instance de la classe Questionnaire avec le fichier JSON
     questionnaire.poser_questions()                  # Pose les questions à l'utilisateur
     questionnaire.afficher_correction()              # Affiche les corrections
-
+    input("Appuyez sur 'Entrée' pour quitter...")
+c
